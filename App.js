934b180be129adef9326ad8f7d0fd5317d785c7c
Marcus Koh <kohchihao@gmail.com>
2022-12-30 15:45:31 +0800

    chore: remove postinstall

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { chunk } from 'lodash';
 
 const App = () => {
-  const result = chunk(['a', 'b', 'c', 'd'], 2);
-  return <Text>Hello World from Feature 123456 {result}</Text>;
+  return <Text>Hello World from Feature</Text>;
 };
 
 export default App;
--- postinstall.js
+++ postinstall.js
@@ -1,33 +1,33 @@
-'use strict';
+// 'use strict';
 
-const fs = require('fs');
-const { exec } = require('child_process');
-let rawdata = fs.readFileSync('host.config.json');
-let hostConfig = JSON.parse(rawdata);
-console.log(hostConfig);
+// const fs = require('fs');
+// const { exec } = require('child_process');
+// let rawdata = fs.readFileSync('host.config.json');
+// let hostConfig = JSON.parse(rawdata);
+// console.log(hostConfig);
 
-if (fs.existsSync(hostConfig.path)) {
-  console.log('Host Directory exists!');
-  let symlinks = hostConfig.symlinks;
-  console.log(symlinks);
-  symlinks.forEach((pkg) => {
-    const cmd = `npm link ${hostConfig.path}/node_modules/${pkg}`;
-    console.log(cmd);
-    exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`error: ${error.message}`);
-        return;
-      }
+// if (fs.existsSync(hostConfig.path)) {
+//   console.log('Host Directory exists!');
+//   let symlinks = hostConfig.symlinks;
+//   console.log(symlinks);
+//   symlinks.forEach((pkg) => {
+//     const cmd = `npm link ${hostConfig.path}/node_modules/${pkg}`;
+//     console.log(cmd);
+//     exec(cmd, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`error: ${error.message}`);
+//         return;
+//       }
 
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-      }
+//       if (stderr) {
+//         console.error(`stderr: ${stderr}`);
+//         return;
+//       }
 
-      console.log(`stdout:\n${stdout}`);
-    });
-  });
-} else {
-  console.log('Host Directory not found.');
-  return;
-}
+//       console.log(`stdout:\n${stdout}`);
+//     });
+//   });
+// } else {
+//   console.log('Host Directory not found.');
+//   return;
+// }
